63c793d2c572cd86faba4abba368a7c39c5c8e6d
HSW425448692 <73380363+HSW42544869@users.noreply.github.com>
2022-03-16 12:01:14 +0800

    googleChrom_Dinosaur final
    大致完成

---
+++ GoogleChrome Dinosaur Game/Assets/Scripts/Dinosaur.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    public float jump;
+    bool isjump;
+    public Gamemanager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isjump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) &&  isjump == false)
+        {
+            rb.velocity = new Vector2(0, jump);
+            isjump = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isjump = false;
+
+        if (collision.gameObject.tag == "Cactus")
+        {
+            gm.GameOver();
+        }
+    }
+}
